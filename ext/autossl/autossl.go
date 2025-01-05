8b3e919f8a947125976fbd2cf96ab3e16947e9e8
Lz <imlangzi@qq.com>
2025-01-05 09:52:55 +0800

    fix(autossl): fixed tls config issues for GSC-G402

--- ext/autossl/autossl.go
+++ ext/autossl/autossl.go
@@ -7,8 +7,20 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
+// New creates and returns two HTTP servers: one for HTTP and one for HTTPS.
+// The HTTP server redirects all traffic to HTTPS using the autocert.Manager's HTTPHandler.
+// The HTTPS server uses the provided ServeMux and autocert.Manager for automatic TLS certificate management.
+//
+// Parameters:
+//
+//	mux - The HTTP request multiplexer to use for the HTTPS server.
+//	opts - A variadic list of Option functions to configure the autocert.Manager.
+//
+// Returns:
+//
+//	*http.Server - The HTTP server configured to redirect traffic to HTTPS.
+//	*http.Server - The HTTPS server configured with automatic TLS certificate management.
 func New(mux *http.ServeMux, opts ...Option) (*http.Server, *http.Server) {
-
 	cm := autocert.Manager{
 		Prompt: autocert.AcceptTOS,
 	}
@@ -30,6 +42,8 @@ func New(mux *http.ServeMux, opts ...Option) (*http.Server, *http.Server) {
 		Addr:    ":https",
 		Handler: mux,
 		TLSConfig: &tls.Config{
+			MinVersion:     tls.VersionTLS12,
+			MaxVersion:     0,
 			GetCertificate: cm.GetCertificate,
 		},
 	}
