7a7fb40b7caed92404a0a601f32221e5e80076e9
Lz <imlangzi@qq.com>
2025-01-23 21:59:59 +0800

    feat(ext): added hsts middleware (#29)

---
+++ ext/hsts/hsts.go
@@ -0,0 +1,67 @@
+package hsts
+
+import (
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/yaitoo/xun"
+)
+
+// Enable sets the Strict-Transport-Security header with the given maxAge,
+// includeSubdomains and preload values.
+//
+// The Strict-Transport-Security header is used to inform browsers that the site
+// should only be accessed over HTTPS, and that any HTTP requests should be
+// automatically rewritten as HTTPS.
+//
+// maxAge is the maximum age of the header in seconds.
+//
+// includeSubdomains will include all subdomains of the current domain in the
+// header.
+//
+// preload will add the preload directive to the header, which allows the site
+// to be included in the HSTS preload list.
+//
+// The HSTS preload list is a list of sites that are known to be HTTPS-only, and
+// are included in the browser's HSTS list by default. This allows the browser to
+// immediately switch to HTTPS for these sites, without having to wait for the
+// first request to complete.
+func Enable(maxAge time.Duration, includeSubdomains, preload bool) xun.Middleware {
+	return func(next xun.HandleFunc) xun.HandleFunc {
+		return func(c *xun.Context) error {
+			r := c.Request()
+
+			if r.TLS == nil && (r.Method == "GET" || r.Method == "HEAD") {
+				target := "https://" + stripPort(r.Host) + r.URL.RequestURI()
+
+				if maxAge <= 0 {
+					maxAge = 365 * 24 * time.Hour
+				}
+
+				v := "max-age=" + strconv.FormatInt(int64(maxAge/time.Second), 10)
+				if includeSubdomains {
+					v += "; includeSubDomains"
+				}
+				if preload {
+					v += "; preload"
+				}
+				c.WriteHeader("Strict-Transport-Security", v)
+
+				c.Redirect(target, http.StatusFound)
+				return xun.ErrCancelled
+			}
+
+			return next(c)
+		}
+	}
+}
+
+func stripPort(hostPort string) string {
+	host, _, err := net.SplitHostPort(hostPort)
+	if err != nil {
+		return hostPort
+	}
+	return host
+}
---
+++ ext/hsts/hsts_test.go
@@ -0,0 +1,130 @@
+package hsts
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/yaitoo/xun"
+)
+
+func TestHstsMiddleware(t *testing.T) {
+	c := http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error { // skipcq: RVV-B0012
+			return http.ErrUseLastResponse
+		},
+	}
+
+	t.Run("max_age_should_work", func(t *testing.T) {
+		mux := http.NewServeMux()
+		srv := httptest.NewServer(mux)
+		defer srv.Close()
+
+		u, err := url.Parse(srv.URL)
+		require.NoError(t, err)
+
+		l := "https://" + u.Hostname() + "/"
+		app := xun.New(xun.WithMux(mux))
+
+		app.Use(Enable(1*time.Hour, false, false))
+
+		app.Get("/", func(c *xun.Context) error {
+			return c.View(nil)
+		})
+
+		req, err := http.NewRequest(http.MethodGet, srv.URL, nil)
+		require.NoError(t, err)
+		resp, err := c.Do(req)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusFound, resp.StatusCode)
+		require.Equal(t, l, resp.Header.Get("Location"))
+		require.Equal(t, "max-age=3600", resp.Header.Get("Strict-Transport-Security")) // default MaxAge is 1 year
+	})
+
+	t.Run("invalid_max_age_should_work", func(t *testing.T) {
+		mux := http.NewServeMux()
+		srv := httptest.NewServer(mux)
+		defer srv.Close()
+
+		u, err := url.Parse(srv.URL)
+		require.NoError(t, err)
+
+		l := "https://" + u.Hostname() + "/"
+		app := xun.New(xun.WithMux(mux))
+
+		app.Use(Enable(0*time.Hour, false, false))
+
+		app.Get("/", func(c *xun.Context) error {
+			return c.View(nil)
+		})
+
+		req, err := http.NewRequest(http.MethodGet, srv.URL, nil)
+		require.NoError(t, err)
+		resp, err := c.Do(req)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusFound, resp.StatusCode)
+		require.Equal(t, l, resp.Header.Get("Location"))
+		require.Equal(t, "max-age=31536000", resp.Header.Get("Strict-Transport-Security"))
+	})
+
+	t.Run("max_age_includesubdomains_should_work", func(t *testing.T) {
+		mux := http.NewServeMux()
+		srv := httptest.NewServer(mux)
+		defer srv.Close()
+
+		u, err := url.Parse(srv.URL)
+		require.NoError(t, err)
+
+		l := "https://" + u.Hostname() + "/"
+		app := xun.New(xun.WithMux(mux))
+
+		app.Use(Enable(1*time.Hour, true, false))
+
+		app.Get("/", func(c *xun.Context) error {
+			return c.View(nil)
+		})
+
+		req, err := http.NewRequest(http.MethodGet, srv.URL, nil)
+		require.NoError(t, err)
+		resp, err := c.Do(req)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusFound, resp.StatusCode)
+		require.Equal(t, l, resp.Header.Get("Location"))
+		require.Equal(t, "max-age=3600; includeSubDomains", resp.Header.Get("Strict-Transport-Security"))
+	})
+
+	t.Run("max_age_includesubdomains_preload_should_work", func(t *testing.T) {
+		mux := http.NewServeMux()
+		srv := httptest.NewServer(mux)
+		defer srv.Close()
+
+		u, err := url.Parse(srv.URL)
+		require.NoError(t, err)
+
+		l := "https://" + u.Hostname() + "/"
+		app := xun.New(xun.WithMux(mux))
+
+		app.Use(Enable(1*time.Hour, true, true))
+
+		app.Get("/", func(c *xun.Context) error {
+			return c.View(nil)
+		})
+
+		req, err := http.NewRequest(http.MethodGet, srv.URL, nil)
+		require.NoError(t, err)
+		resp, err := c.Do(req)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusFound, resp.StatusCode)
+		require.Equal(t, l, resp.Header.Get("Location"))
+		require.Equal(t, "max-age=3600; includeSubDomains; preload", resp.Header.Get("Strict-Transport-Security"))
+	})
+
+	t.Run("without_port_should_work", func(t *testing.T) {
+		host := stripPort("abc.com")
+
+		require.Equal(t, "abc.com", host)
+	})
+}
