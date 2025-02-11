9c23fef46d26d4adf2e39d8e8582426a0e7596ef
Lz <imlangzi@qq.com>
2025-02-11 22:29:15 +0800

    fix(hsts): added StartsWith rule (#44)

--- ext/hsts/option.go
+++ ext/hsts/option.go
@@ -44,12 +44,38 @@ func WithPreload() Option {
 	}
 }
 
+// IgnoreRule is a function that takes a pointer to an http.Request
+// and returns a boolean indicating whether the request should be
+// ignored by the HSTS middleware.
 type IgnoreRule func(*http.Request) bool
 
-func Ignore(paths ...string) IgnoreRule {
+// Match creates an IgnoreRule that matches the given paths to ignore requests.
+//
+// The paths are matched case-insensitively, so "/Doc" and "/doc" would be equivalent.
+func Match(paths ...string) IgnoreRule {
 	return func(r *http.Request) bool {
 		for _, path := range paths {
 			if strings.EqualFold(r.URL.Path, path) {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+// StartsWith creates an IgnoreRule that checks if the request path starts with any of the specified prefixes.
+//
+// The provided paths are automatically converted to lower-case for consistent matching.
+func StartsWith(paths ...string) IgnoreRule {
+	// Convert provided paths to lower-case once for consistent matching.
+	lowerPaths := make([]string, len(paths))
+	for i, p := range paths {
+		lowerPaths[i] = strings.ToLower(p)
+	}
+	return func(r *http.Request) bool {
+		l := strings.ToLower(r.URL.Path)
+		for _, path := range lowerPaths {
+			if strings.HasPrefix(l, path) {
 				return true
 			}
 		}
