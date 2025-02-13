6ed52e2c2153342a471520e4a8b9a5aa5daa83da
Lz <imlangzi@qq.com>
2025-02-13 11:57:17 +0800

    fix(crsf): added etag on csrf.js

--- ext/csrf/csrf.go
+++ ext/csrf/csrf.go
@@ -2,10 +2,14 @@ package csrf
 
 import (
 	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
 	"embed"
+	"encoding/base64"
 	"html/template"
 	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/yaitoo/xun"
@@ -51,7 +55,7 @@ func New(secretKey []byte, opts ...Option) xun.Middleware {
 //go:embed csrf.js
 var fsys embed.FS
 
-var lastModified = time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
+var zeroTime time.Time
 
 func LoadJsToken(opts ...Option) xun.HandleFunc {
 	o := &Options{
@@ -71,10 +75,26 @@ func LoadJsToken(opts ...Option) xun.HandleFunc {
 	var processed bytes.Buffer
 	t.Execute(&processed, o) // nolint: errcheck
 
+	mac := hmac.New(sha256.New, o.SecretKey)
+	mac.Write(processed.Bytes())
+
+	etag := base64.URLEncoding.EncodeToString(mac.Sum(nil))
+
 	return func(c *xun.Context) error {
+		if match := c.Request.Header.Get("If-None-Match"); match != "" {
+			for _, it := range strings.Split(match, ",") {
+				if strings.TrimSpace(it) == etag {
+					c.Response.WriteHeader(http.StatusNotModified)
+					return nil
+				}
+			}
+		}
+
+		c.Response.Header().Set("ETag", etag)
+
 		content := bytes.NewReader(processed.Bytes())
 		c.Response.Header().Set("Content-Type", "application/javascript")
-		http.ServeContent(c.Response, c.Request, "csrf.js", lastModified, content)
+		http.ServeContent(c.Response, c.Request, "csrf.js", zeroTime, content)
 		return nil
 	}
 }
